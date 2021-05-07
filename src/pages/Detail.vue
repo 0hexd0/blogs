fdfbb10fcf7fd253211cc7e4ce194435c05a2421
HeXudong <hexudong@gongguizhijia.com>
2021-05-07 15:27:05 +0800

    feat: markdown更多特性

--- src/pages/Detail.vue
+++ src/pages/Detail.vue
@@ -2,7 +2,7 @@
   <div class="pure-g">
     <div class="pure-u-1-8"></div>
     <div class="pure-u-3-4">
-      <div v-html="content"></div>
+      <div id="md-content" v-html="content"></div>
     </div>
     <div class="pure-u-1-8"></div>
   </div>
@@ -13,6 +13,7 @@ import { defineComponent } from "vue";
 import { apis } from "Apis/index";
 import marked from "marked";
 import hljs from "highlight.js";
+import katex from "katex";
 
 // Set options
 // `highlight` example uses `highlight.js`
@@ -24,13 +25,33 @@ marked.setOptions({
   langPrefix: "hljs ",
   pedantic: false,
   gfm: true,
-  tables: true,
   breaks: false,
   sanitize: false,
   smartLists: true,
   smartypants: false,
   xhtml: false,
 });
+
+// Override function
+const tokenizer = {
+  codespan(src: string) {
+    // const match = src.match(/\$\$([^\$\n]+?)\$\$/);
+    const match = src.match(/\$\$(.+?)\$\$/);
+    if (match) {
+      console.log("match[0]", match[0].trim());
+      return {
+        type: "text",
+        raw: match[0],
+        text: katex.renderToString(match[1].trim()),
+      };
+    }
+
+    // return false to use original codespan tokenizer
+    return false;
+  },
+} as any;
+
+marked.use({ tokenizer });
 
 export default defineComponent({
   components: {},
@@ -52,5 +73,60 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+/* for md  */
+#md-content {
+  code {
+    border: 1px solid #bbb;
+    border-radius: 2px;
+  }
+
+  code.hljs {
+    font-size: 15px;
+  }
+
+  blockquote {
+    color: #666;
+    border-left: 4px solid #ddd;
+    padding-left: 20px;
+    margin-left: 0;
+    font-style: italic;
+  }
+
+  code {
+    border: 1px solid #ddd;
+    background: #f6f6f6;
+    padding: 3px;
+    border-radius: 3px;
+    font-size: 14px;
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+    display: block;
+    width: 100%;
+    overflow: auto;
+    word-break: normal;
+    margin: 1em 0;
+  }
+
+  thead tr {
+    background-color: #f8f8f8;
+  }
+
+  tbody tr:nth-child(2n) {
+    background-color: #f8f8f8;
+  }
+
+  th {
+    padding: 6px 13px;
+    border: 1px solid #ddd;
+  }
+
+  td {
+    padding: 6px 13px;
+    border: 1px solid #ddd;
+  }
+}
 </style>
